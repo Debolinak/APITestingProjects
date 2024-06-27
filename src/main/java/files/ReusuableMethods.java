df9064e6ab08957790422a53fdfdfd52c8b74e89
Debolina <debolinatester@gmail.com>
2024-06-27 11:51:58 +0530

    interview Commit

---
+++ src/main/java/files/ReusuableMethods.java
@@ -0,0 +1,11 @@
+package files;
+
+import io.restassured.path.json.JsonPath;
+
+public class ReusuableMethods {
+    public static JsonPath rawToJSon(String response)
+    {
+    JsonPath js1 = new JsonPath(response);
+            return js1;
+    }
+}
